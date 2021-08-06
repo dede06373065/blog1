f72fff9d9cb5282c00026c311bee788e09af2aa8
Jiazhen Guo <Jen06373065@163.com>
2021-08-06 19:49:20 +0930

    cookie safty

--- src/router/user.js
+++ src/router/user.js
@@ -1,5 +1,12 @@
 const { login } = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
+//获取cookie的过期时间
+const getCookieExpire=()=>{
+    const d=new Date()
+    d.setTime(d.getTime()+(24*60*60*1000))
+    return d.toGMTString()
+
+}
 const handleUserRouter = (req, res) => {
     const method = req.method
 
@@ -8,7 +15,7 @@ const handleUserRouter = (req, res) => {
         const result = login(username, password)
         return result.then(data=>{
             if (data.username) {
-                res.setHeader('Set-Cookie',`username=${data.username}; path=/`)
+                res.setHeader('Set-Cookie',`username=${data.username}; path=/ ; httpOnly; expires='${getCookieExpire()}'`)
                 return new SuccessModel()
             }
             return new ErrorModel()
